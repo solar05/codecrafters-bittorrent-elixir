36e4f71da3a6fef07bbbefe3b4457b5c95e3e11c
solar05 <artem00298@gmail.com>
2024-10-10 03:25:55 +0300

    Add number parsing

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -3,11 +3,11 @@ defmodule Bittorrent.CLI do
     case argv do
       ["decode" | [encoded_str | _]] ->
         # You can use print statements as follows for debugging, they'll be visible when running tests.
-        IO.puts("Logs from your program will appear here!")
+        # IO.puts("Logs from your program will appear here!")
 
-      # Uncomment this block to pass the first stage
-      # decoded_str = Bencode.decode(encoded_str)
-      # IO.puts(Jason.encode!(decoded_str))
+        decoded_str = Bencode.decode(encoded_str)
+        IO.puts(Jason.encode!(decoded_str))
+
       [command | _] ->
         IO.puts("Unknown command: #{command}")
         System.halt(1)
@@ -23,15 +23,25 @@ defmodule Bencode do
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
 
-    case Enum.find_index(binary_data, fn char -> char == 58 end) do
-      nil ->
-        IO.puts("The ':' character is not found in the binary")
+    if encoded_number?(binary_data) do
+      Enum.slice(binary_data, 1..(Enum.count(binary_data) - 2)) |> List.to_string()
+    else
+      case Enum.find_index(binary_data, fn char -> char == 58 end) do
+        nil ->
+          IO.puts("The ':' character is not found in the binary")
 
-      index ->
-        rest = Enum.slice(binary_data, index+1..-1)
-        List.to_string(rest)
+        index ->
+          rest = Enum.slice(binary_data, (index + 1)..-1)
+          List.to_string(rest)
+      end
     end
   end
 
   def decode(_), do: "Invalid encoded value: not binary"
+
+  defp encoded_number?(encoded_value) do
+    List.first(encoded_value) == 105 && List.last(encoded_value) == 101
+  end
+
+  # Enum.slice(encoded_value, 1..Enum.count(encoded_value - 1)) |> List.to_string()
 end
